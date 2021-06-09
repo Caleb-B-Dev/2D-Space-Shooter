1e208fe9c0751eeb2ee13ec94f57bd4a8b911d0b
Caleb Breuner <cbreuner@nscrypt.local>
2021-06-09 00:33:02 -0400

    enemis_c revert back to b

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -27,12 +27,6 @@ public class Enemy : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            Player player = other.transform.GetComponent<Player>();
-
-            if(player != null)
-            {
-                player.Damage();
-            }
             Destroy(this.gameObject);
         }
 
