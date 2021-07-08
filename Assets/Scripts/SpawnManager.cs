903dd744f6a69f1b19ea02cb5c8115c34a69aa95
Caleb Breuner <cbreuner@nscrypt.local>
2021-07-07 23:49:07 -0400

    Spawn Enemies Coroutine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -27,9 +27,7 @@ public class SpawnManager : MonoBehaviour
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7.6f, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
-        }
-        
+        }  
     }
 }
